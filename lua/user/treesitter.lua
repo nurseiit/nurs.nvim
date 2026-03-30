d6fff84db32c4bf3e1695f9c6a4d1d57a9f545cd
Nurseiit Abdimomyn <nurseiit.abd@gmail.com>
2026-03-31 00:37:39 +0100

    update all and fix treesitter to load from main

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,55 +1,37 @@
 local M = {
-  -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
 }
 
+local parsers = {
+  "c",
+  "cpp",
+  "go",
+  "lua",
+  "python",
+  "rust",
+  "toml",
+  "tsx",
+  "javascript",
+  "jsdoc",
+  "typescript",
+  "vimdoc",
+  "vim",
+  "bash",
+  "graphql",
+  "scss",
+  "css",
+}
+
 function M.config()
-  -- See `:help nvim-treesitter`
-  -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-  vim.defer_fn(function()
-    require("nvim-treesitter.configs").setup {
-      -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = {
-        "c",
-        "cpp",
-        "go",
-        "lua",
-        "python",
-        "rust",
-        "toml",
-        "tsx",
-        "javascript",
-        "jsdoc",
-        "typescript",
-        "vimdoc",
-        "vim",
-        "bash",
-        "graphql",
-        "scss",
-        "css",
-      },
+  require("nvim-treesitter").install(parsers)
 
-      -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-      auto_install = false,
-
-      sync_install = false,
-      ignore_install = {},
-      modules = {},
-
-      highlight = { enable = true },
-      indent = { disable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<c-space>",
-          node_incremental = "<c-space>",
-          scope_incremental = "<c-s>",
-          node_decremental = "<M-space>",
-        },
-      },
-    }
-  end, 0)
+  vim.api.nvim_create_autocmd("FileType", {
+    callback = function()
+      pcall(vim.treesitter.start)
+    end,
+  })
 
   vim.filetype.add {
     extension = {
